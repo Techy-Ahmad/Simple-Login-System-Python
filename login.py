62e6200758396fbc494aa4162533b15a8ce4b923
Techy_Ahmad <ma.ahmed.cs@gmail.com>
2026-04-20 00:26:37 +0500

    Refactor login logic into a separate function

--- login.py
+++ login.py
@@ -1,21 +1,25 @@
-username = "Admin"
-password = "12345"
-
-attempts = 0
-
-for i in range(1, 4):
-    print("Attempt", i, "/ 3")
-
-    user_input = input("Enter username: ")
-    pass_input = input("Enter password: ")
-
-    attempts += 1
-
-    if user_input == username and pass_input == password:
-        print("Login Successful")
-        break
+def enter_info(user,pw):
+    username="Admin"
+    password="12345"
+    if user==username and pw==password:
+            return True
     else:
-        print("Invalid credentials")
+            return False
+    
+attempts = 0
+for i in range(1,4):
+    print("Attempt",i," / 3")
 
-if attempts == 3 and user_input != username or pass_input != password:
-    print("Account Locked")
+    user_input=input("Enter your Username: ")
+    pass_input=input("Enter your Password: ")
+
+    result=enter_info(user_input,pass_input)
+    if result:
+      print("login Success")
+      break
+    else:
+      print("Login Denied")
+    attempts+=1
+
+if attempts==3:
+    print("account locked")
